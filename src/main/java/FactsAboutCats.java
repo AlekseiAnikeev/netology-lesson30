5ba4c4abbe6215e46383c83448056de46e88122e
AlekseiAnikeev <info.anikeev@gmail.com>
2022-09-28 23:25:30 +0300

    init commit

---
+++ src/main/java/FactsAboutCats.java
@@ -0,0 +1,40 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Aleksey Anikeev aka AgentChe
+ * Date of creation: 28.09.2022
+ */
+public class FactsAboutCats {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public FactsAboutCats(@JsonProperty("id") String id,
+                          @JsonProperty("text") String text,
+                          @JsonProperty("type") String type,
+                          @JsonProperty("user") String user,
+                          @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "FactsAboutCats{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
---
+++ src/main/java/HttpResponse.java
@@ -0,0 +1,39 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Aleksey Anikeev aka AgentChe
+ * Date of creation: 28.09.2022
+ */
+public class HttpResponse {
+    private static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        List<FactsAboutCats> factsAboutCats = gettingDataAboutCats();
+        factsAboutCats.forEach(System.out::println);
+    }
+
+    private static List<FactsAboutCats> gettingDataAboutCats() {
+        List<FactsAboutCats> responseList;
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet(URL);
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                responseList = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return responseList.stream()
+                .filter(upvotes -> upvotes.getUpvotes() > 0)
+                .toList();
+    }
+}
